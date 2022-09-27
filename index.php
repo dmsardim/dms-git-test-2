5580741f6071364522b60270f51b12188eee6a3b
dmsardim <dimasardiyanto19@gmail.com>
2022-09-27 04:59:49 -0500

    menambahkan paragraf

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>COba Git Remote</title>
+    <title>Coba Git Remote</title>
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quae rem nobis laudantium sed suscipit expedita soluta dolor, molestias quam voluptatem? Explicabo quam quibusdam quaerat aperiam! Impedit ut quam saepe.</p>
 </body>
 </html>
